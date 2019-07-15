000f1ff2dff3182e0fbed736c52bf023638a252c
Dawn <1401629898@qq.com>
2019-07-15 10:56:44 +0800

    TypeOf

--- index.js
+++ index.js
@@ -10,16 +10,18 @@ export const TypeOf = obj => {
     //toString会返回对应不同标签的构造函数
     let toString = Object.prototype.toString
     let map = {
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
-      '[]': '',
+      '[object Boolean]': 'boolean',
+      '[object Number]': 'number',
+      '[object String]': 'string',
+      '[object Function]': 'function',
+      '[object Array]': 'array',
+      '[object Date]': 'date',
+      '[object RegExp]': 'regExp',
+      '[object Undefined]': 'undefined',
+      '[object Null]': 'null',
+      '[object Object]': 'object',
     }
+    return map[toString.call(obj)]
   }
 }
+
