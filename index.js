5153a3365b0737d2b21f6432c377041d640cb3f2
Dawn <1401629898@qq.com>
2019-07-15 10:49:33 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * @description 判断数据类型
+ * @param {*} obj 传入数据
+ * @returns String
+ */
+export const TypeOf = obj => {
+  if (obj instanceof Element) {
+    return 'Element'
+  } else {
+    //toString会返回对应不同标签的构造函数
+    let toString = Object.prototype.toString
+    let map = {
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+      '[]': '',
+    }
+  }
+}
